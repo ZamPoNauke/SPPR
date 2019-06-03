c7e0628fdf1b30f0d08393ced0be68e1c17b56fe
ZamPoNauke <46603598+ZamPoNauke@users.noreply.github.com>
2019-06-04 02:07:37 +0500

    v 0.02

--- Lab4/Release/FBaesa_1.py
+++ Lab4/Release/FBaesa_1.py
@@ -2,32 +2,40 @@
 def open_file(massiv, file):
     with open(file) as handle:  # открываем файл с гипотезами
         for line in handle:
-            massiv.append([str(x) for x in line.split()])
+            massiv.append([str(x) for x in line.split('; ')])
+    for i in massiv:
+        i[-1] = i[-1][:-1]
     return massiv
 
+
+def print_masiv(masiv):
+    for i in masiv:
+        print(i)
+    print()
+
+
 def main():
-    hypothesis = []
-    svid = []
+    hypothesis = []  # гипотезы
+    certificate = []  # свидетельства
     open_file(hypothesis, "hypothesis.txt")
     for i in hypothesis:
         i[1] = float(i[1])
         i[4] = float(i[1])
         i[5] = float(i[5])
-    open_file(svid, "certificate.txt")
-    print(hypothesis, '\n', svid)
+    open_file(certificate, "certificate.txt")
+    print_masiv(hypothesis)
+    print_masiv(certificate)
 
     n = 0
-    while n < len(svid):
-        Price = 0  # цена (подсчет цен). Нужны для того, что бы определить какой вопрос задать первым.
-        N = 0
-
-        for i in hypothesis: # Высч
+    while n < len(certificate):
+        for i in hypothesis:
+            # цена (подсчет цен). Нужны для того, что бы определить какой вопрос задать первым.
             Price = abs(((i[4]*i[1]) / (i[4]*i[1]+i[5]*(1-i[1]))) - (((1-i[4])*i[1]) / (1-i[4]*i[1]-i[5]*(1-i[1]))))
             N = i[3]
-            for j in svid:
+            for j in certificate:
                 if j[0] == N:
                     j[2] = Price  # записываю цену в список свидетельств
-        #print(svid)
+        print_masiv(certificate)
 
         for i in hypothesis:
             PHE = (i[4]*i[1])/(i[4]*i[1]+i[5]*(1-i[1]))
@@ -35,9 +43,9 @@ def main():
             PH_E = ((1-i[4])*i[1])/(1-i[4]*i[1]-i[5]*(1-i[1]))
             i[7] = PH_E
 
-    #  выбор вопроса по цене
+        #  выбор вопроса по цене
         max_list = []
-        for i in svid:
+        for i in certificate:
             if i[3] == '0':
                 max_list.append(i[2])
                 max_ce = max(max_list)
@@ -46,33 +54,23 @@ def main():
                     svid_pred_num = i[0]
 
         print(svid_pred)
-        yn = input("Да/Нет/Не знаю: ")
-        yn = yn.lower()
+        yn = (input("Да, Нет или Не знаю: ")).lower()
+
+        def unswer_hyp(svid_pred_num, hypothesis, data):
+            for i in hypothesis:
+                if i[3] == svid_pred_num:
+                    i[1] = i[data]
 
         if yn == 'да':
-            for i in hypothesis:
-                if i[3] == svid_pred_num:
-                    i[1] = i[6]
-            for i in svid:
-                if svid_pred_num == i[0]:
-                    i[3] = '1'
+            unswer_hyp(svid_pred_num, hypothesis, 6)
         elif yn == 'нет':
-            for i in hypothesis:
-                if i[3] == svid_pred_num:
-                    i[1] = i[7]
-            for i in svid:
-                if svid_pred_num == i[0]:
-                    i[3] = '1'
-        else:
-            for i in svid:
-                if svid_pred_num == i[0]:
-                    i[3] = '1'
+            unswer_hyp(svid_pred_num, hypothesis, 7)
+        for i in certificate:
+            if svid_pred_num == i[0]:
+                i[3] = '1'
 
         n += 1
-        Ph_1 = 0.0
-        Ph_2 = 0.0
-        Ph_3 = 0.0
-        Ph_4 = 0.0
+        Ph_1, Ph_2, Ph_3, Ph_4 = 0.0, 0.0, 0.0, 0.0
         Ph_list = []
         for i in hypothesis:
             if i[2] == '1':
@@ -83,24 +81,15 @@ def main():
                 Ph_3 += i[1]
             if i[2] == '4':
                 Ph_4 += i[1]
-        Ph_list.append(Ph_1)
-        Ph_list.append(Ph_2)
-        Ph_list.append(Ph_3)
-        Ph_list.append(Ph_4)
-        Ph_list_film = ['Припять', 'Мальчишник', 'Виноваты_звезды', 'Горько']
+        Ph_list.extend([Ph_1, Ph_2, Ph_3, Ph_4])
+        Ph_list_film = []
         for i in hypothesis:
-            max_Ph = Ph_list.index(max(Ph_list))
-            max_Ph = str(max_Ph)
+            if i[0] not in(Ph_list_film):
+                Ph_list_film.append(i[0])
 
-    if max_Ph == '0':
-        print(Ph_list_film[0])
-    elif max_Ph == '1':
-        print(Ph_list_film[1])
-    elif max_Ph == '2':
-        print(Ph_list_film[2])
-    else:
-        print(Ph_list_film[3])
+        max_Ph = int(Ph_list.index(max(Ph_list)))
 
+    print("Ответ:", Ph_list_film[max_Ph])
 
 if __name__ == '__main__':
     main()
